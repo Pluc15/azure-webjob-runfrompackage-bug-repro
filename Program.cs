cba8de5c3263233ecc946f89fb3f35cb96ee4953
Pier-Luc Michaud <pluc.m@outlook.com>
2019-04-25 11:12:54 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SomeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var running = true;
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (s, e) =>
+            {
+                running = false;
+                cts.Cancel();
+            };
+            while (running)
+            {
+                Console.WriteLine("Hello World!");
+                Task.Delay(1000, cts.Token).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
